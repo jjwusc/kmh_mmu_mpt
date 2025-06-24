83acea820c2e6488f9e99d7d38e68f7f43ab8067
jjwusc <157932076+jjwusc@users.noreply.github.com>
2025-06-24 10:30:38 +0800

    pmppma,missRate

--- src/arch/riscv/RiscvTLB.py
+++ src/arch/riscv/RiscvTLB.py
@@ -94,7 +94,11 @@ class RiscvTLB(BaseTLB):
     initial_back_pre_precision_value = Param.Bool(False,"initial value of back_pre_precision")
     initial_forward_pre_precision_value = Param.Bool(False,"initial value of forward_pre_precision")
     #JJW:
-    mptcache_size = Param.Int(128, "MPT Cache size passed to C++")
+    mptcache_l0_size = Param.Int(32, "MPTCache L0 size")
+    mptcache_l1_size = Param.Int(32, "MPTCache L1 size")
+    mptcache_l2_size = Param.Int(32, "MPTCache L2 size")
+    mptcache_l3_size = Param.Int(32, "MPTCache L3 size")
+    mptcache_sp_size = Param.Int(32, "MPTCache SP size")
 
 
 class RiscvTLBL2(RiscvTLB):
